75344403d93630615b0a7230c39dd23534f12ce6
Andrés Jácome <whitedev93@gmail.com>
2025-03-06 03:07:36 -0500

    Updated docker-compose

--- frontend/src/environments/environment.template.ts
+++ frontend/src/environments/environment.template.ts
@@ -1,15 +1,18 @@
 export const environment = {
     api: {
-        urls: {
-            serviceUsers: '${GATEWAY_ENDPOINT:-http://localhost:8000}/users-service',
-            serviceCourses: '${GATEWAY_ENDPOINT:-http://localhost:8000}/courses-service',
-        }
+      urls: {
+        serviceUsers: '${GATEWAY_ENDPOINT:-http://localhost:8080}/user-service',
+        serviceProducts: '${GATEWAY_ENDPOINT:-http://localhost:8080}/product-service',
+        serviceOrders: '${GATEWAY_ENDPOINT:-http://localhost:8080}/order-service',
+        serviceInventory: '${GATEWAY_ENDPOINT:-http://localhost:8080}/inventory-service',
+        serviceCourses: '${GATEWAY_ENDPOINT:-http://localhost:8080}/courses-service',
+      }
     },
     keycloak: {
-        config: {
-            url: '${KEYCLOAK_URL:-http://localhost:8003/auth}',
-            realm: '${KEYCLOAK_REALM:-SpringBootKeycloak}',
-            clientId: '${KEYCLOAK_CLIENT_ID:-courses-app}',
-        }
+      config: {
+        url: '${KEYCLOAK_URL:-http://localhost:8090/auth}',
+        realm: '${KEYCLOAK_REALM:-SpringBootKeycloak}',
+        clientId: '${KEYCLOAK_CLIENT_ID:-EcovidaClient}',
+      }
     }
-};
+  };
